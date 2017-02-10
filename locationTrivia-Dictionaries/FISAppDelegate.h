7f66e9419b53c500babf0b74574915f0ab9e5c2c
Rama Milaneh <ramamilaneh@gmail.com>
2017-02-10 18:29:26 -0500

    Done

--- locationTrivia-Dictionaries/FISAppDelegate.h
+++ locationTrivia-Dictionaries/FISAppDelegate.h
@@ -6,10 +6,20 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-/**
+
  
- *  Declare your method names here.
+-(NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location toLength:(NSUInteger)length;
+
+-(NSDictionary *)dictionaryForLocationWithName:(NSString *)name latitude:(CGFloat)latitude longitude:(CGFloat)longitude;
+
  
- */
+-(NSArray *)namesOfLocations:(NSArray *)locations;
+
+-(BOOL)dictionaryIsValidLocation:(NSDictionary *)location;
+
+-(NSDictionary *)locationNamed:(NSString *)name inLocations:(NSArray *)locations;
+
+
+
 
 @end
--- locationTrivia-Dictionaries/FISAppDelegate.m
+++ locationTrivia-Dictionaries/FISAppDelegate.m
@@ -10,10 +10,64 @@
     return YES;
 }
 
-/** 
+-(NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location toLength:(NSUInteger)length {
+    NSString *name = location[@"name"];
+    if(length > name.length) {
+        return name;
+    }
+    
+    return [name substringToIndex:length];
+}
+
+-(NSDictionary *)dictionaryForLocationWithName:(NSString *)name latitude:(CGFloat)latitude longitude:(CGFloat)longitude {
+    NSDictionary *trivia = @{@"name":name,@"latitude":@(latitude),@"longitude":@(longitude)};
+    return trivia;
+}
+
+
+-(NSArray *)namesOfLocations:(NSArray *)locations {
+    NSMutableArray *namesOfLocations = [NSMutableArray array];
+    for(NSInteger i = 0;i<[locations count];i++){
+        [namesOfLocations addObject:locations[i][@"name"]];
+    }
+    return namesOfLocations;
+}
+
+-(BOOL)dictionaryIsValidLocation:(NSDictionary *)location {
+    
+    if (location.count != 3) {
+        return NO;
+    }
+    
+    
+    NSString *name = location[@"name"];
+    if (!name) {
+        return NO;
+    }
+    
+    NSNumber *latitudeNumber = location[@"latitude"];
+    if (!latitudeNumber) {
+        return NO;
+    }
+    
+    NSNumber *longitudeNumber = location[@"longitude"];
+    if (!longitudeNumber) {
+        return NO;
+    }
+    
+    
+    return YES;
  
- *  Write your method implementations here.
- 
- */
+}
+
+-(NSDictionary *)locationNamed:(NSString *)name inLocations: (NSArray *)locations {
+    for (NSUInteger i = 0; i<[locations count]; i++) {
+        if([locations[i][@"name"] isEqualToString:name]){
+            return locations[i];
+        }
+    }
+    return nil;
+}
+
 
 @end
